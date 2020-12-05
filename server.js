2d7dcebb56da5deba19d462a686b38cf79e37ad4
dg <danigaglia89@gmail.com>
2020-12-05 02:12:55 +0100

    fix cors origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: process.env.CORS_ORIGIN || "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
